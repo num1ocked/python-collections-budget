9a02e3fa98bc9bd47f5306b55b6a3def4190f5fc
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-18 16:09:55 +0100

    6

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -37,8 +37,9 @@ class BudgetList():
         for expense in expenses.list:
             myBudgetList.append(expense.amount)
         print('The count of all expenses: '+str(len(myBudgetList)))
-        for entry in myBudgetList:
-            print(entry)
+
+    for entry in myBudgetList:
+        print(entry)
 
     if (__name__ == "__main__"):
         main()
