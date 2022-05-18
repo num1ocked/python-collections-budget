65e6814df944034b1d38fa641b860add099dc943
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-18 15:58:01 +0100

    1

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -7,7 +7,6 @@ class BudgetList():
         self.expenses = []
         self.sum_overages = 0
         self.overages = []
-        self.__iter__ = pass
 
     def append(self, item):
         if (self.sum_expenses+item<self.budget):
