9d83b87ce6f67c408c88ea925afa6c71a19b56e4
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-18 16:24:04 +0100

    9

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -43,6 +43,7 @@ def main():
 
     fig, ax = plt.subplots()
     labels = ['Expenses', 'Overages', 'Budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
 
     if (__name__ == "__main__"):
         main()
