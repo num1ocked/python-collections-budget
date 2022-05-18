099022c3c22c3cc630087002d06c94fe097dbb4c
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-18 17:13:06 +0100

    4

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -14,7 +14,7 @@ def main():
         if not (a.issubset(b) and
                 b.issubset(a)):
                 print("Sets are NOT equal by subset test")
-        timeit.timeit(stmt = "expenses.categorize_for_loop()",
+        print(timeit.timeit(stmt = "expenses.categorize_for_loop()",
                     setup =
                     '''
                     from . import Expense
@@ -22,7 +22,7 @@ def main():
                     expenses.read_expenses('data/spending_data.csv')
                     ''',
                     number = 100000,
-                    globals=globals())
+                    globals=globals()))
 
 if __name__ == "__main__":
     main()
