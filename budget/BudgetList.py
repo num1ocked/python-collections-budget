331a60ebf6c3953a218f8192b750183f3149308d
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-18 14:54:58 +0100

    5

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -13,3 +13,6 @@ class BudgetList():
         else:
             self.overages.append(item)
             self.sum_overages+=item
+
+    def __len__(self):
+        return (len(self.expenses)+len(self.overages))
