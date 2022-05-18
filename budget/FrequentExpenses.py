f660e032299f08e4451468f2a4e6418f5d9647bc
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-18 12:14:14 +0100

    step 4

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,3 +1,4 @@
+import collections
 from . import Expense
 
 expenses = Expense.Expenses()
@@ -5,3 +6,5 @@ expenses.read_expenses('data/spending_data.csv')
 spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
+
+spending_counter = collections.Counter(spending_categories)
