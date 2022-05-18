cadb30cf8d05ed8d8066049b39cdf4dcaeabfbb0
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-18 16:45:55 +0100

    3

--- budget/Expense.py
+++ budget/Expense.py
@@ -47,4 +47,4 @@ class Expenses():
                             x.category=='Classes' or x.category=='Utilities' or
                             x.category=='Mortgage'}
         food_expenses = {x for x in self.list
-                        if x.category=='Groceries' or x.category=='Eating Out'}
+                        if x.category == 'Groceries' or x.category == 'Eating Out'}
