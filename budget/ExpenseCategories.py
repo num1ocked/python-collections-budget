f75d380152cf1b6938512bb11e38fadd6b30174d
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-18 17:22:10 +0100

    9

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -36,6 +36,8 @@ def main():
     fig, ax = plt.subplots()
     labels = ['Necessary', 'Food', 'Unnecessary']
     divided_expenses_sum = []
+    for category_exps in divided_set_comp:
+        divided_expenses_sum.append(sum(x.amount for x in category_exps))
 
 if __name__ == "__main__":
     main()
